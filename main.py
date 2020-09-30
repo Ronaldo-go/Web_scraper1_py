05968a6150277bc52f272c716d68a16eb60ede4f
Ronaldo-go <71479472+Ronaldo-go@users.noreply.github.com>
2020-09-29 20:58:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,17 +15,17 @@ def _news_scraper(news_site_uid):
 
     for link in homepage.article_links:
         print(link)
-    if __name__ == "__main__":
-        parser = argparse.ArgumentParser()
+if __name__ == "__main__":
+     parser = argparse.ArgumentParser()
 
-        news_sites_choices = list(config()['news_sites'].keys())
-        parser.add_argument('news_site',
-                            help='The new site that you want to scrape',
-                            type=str,
-                            choices=news_sites_choices)
+     news_sites_choices = list(config()['news_sites'].keys())
+     parser.add_argument('news_site',
+                         help='The new site that you want to scrape',
+                         type=str,
+                         choices=news_sites_choices)
         
-        args = parser.parse_args()
-        _news_scraper(args.news_site)
+     args = parser.parse_args()
+     _news_scraper(args.news_site)
 
 
 
